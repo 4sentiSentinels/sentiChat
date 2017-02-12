515dbf22c45353241218428f91994aa1e60b60fa
noobg1 <jeevu.art@gmail.com>
2017-02-12 17:25:07 +0530

    integrated with mongodb

--- app.js
+++ app.js
@@ -2,38 +2,61 @@ var express = require('express'),
 	app = express(),
 	server = require('http').createServer(app),
 	io = require('socket.io').listen(server),
-	request = require("request"),
+	request = require('request'),
+	mongoose = require('mongoose');
 	users = {};
+
+const bodyParser = require('body-parser')
+
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 
 app.use(express.static('public'))
 server.listen(3000);
+
+mongoose.connect('mongodb://localhost/chat', function(error) {
+	if(error){
+		consoel.log(error);
+	}	else {
+		console.log('connected to db');
+	}
+});
+
+let chatSchema = mongoose.Schema({
+	nick: String,
+	msg: String,
+	created: {type: Date, default: Date.now}
+});
+
+let Chat = mongoose.model('Message', chatSchema);
 
 
 app.get('/',function(req,res){
 	res.sendfile(__dirname + '/index.html');
 });
 
-// function makeRequest(data, socket.nickname) {
-// 	if( /^[a-zA-Z( )]+$/.test( data )) data = data;
-// 		else if ((data) == "") data = 'empty message';
-// 		else data = 'empty message';
-// 		var url = "http://noobg1.pythonanywhere.com/q/"+data, senti, sentiParsed
+app.get('/match',function(req,res){
+	let query = Chat.find({});
+	query.sort('-created').exec(function(error, docs) {
+	if(error) throw error;
+	console.log('loaded');
+	res.render(__dirname + '/views/match', {docs});
+	});
+	//res.sendfile(__dirname + '/match.html');
+});
 
-// 		request(url, function(error, response, body) {
-// 			if(error) {
-// 				console.log("Api call failed!")
-// 				io.sockets.emit('new message',{msg : data, nick : socket.nickname, sentiment : ""});
-// 			}
-// 				else {
-// 			 senti = JSON.parse(body)
-// 			 sentiParsed = String(senti['result'])
-// 			console.log(sentiParsed);
-// 		  io.sockets.emit('new message',{msg : data, nick : socket.nickname, sentiment : sentiParsed});
-// 		}
-// 		});
-// }
 
 io.sockets.on('connection',function(socket){
+	let query = Chat.find({});
+	query.sort('-created').limit(4).exec(function(error, docs) {
+		if(error) throw error;
+		console.log('loaded');
+		socket.emit('load old messages', docs);
+	});
+
 	socket.on('new user',function(data, callback){
 		if(data in users){
 			callback(false);
@@ -65,23 +88,28 @@ io.sockets.on('connection',function(socket){
 				callback('Please enter a message!');
 			}
 		}else {
-			if( /^[a-zA-Z( )]+$/.test( data )) data = data;
-			else if ((data) == "") data = 'empty message';
-			else data = 'empty message';
-			var url = "http://noobg1.pythonanywhere.com/q/"+data, senti, sentiParsed
+			let newMsg = new Chat({msg: data, nick: socket.nickname});
+			newMsg.save(function(error) {
+				if(error) throw error;
+				if( /^[a-zA-Z0-9( )]+$/.test( data )) data = data;
+				else if ((data) == "") data = 'empty message';
+				else data = 'empty message';
+				var url = "http://noobg1.pythonanywhere.com/q/"+data, senti, sentiParsed
 
-			request(url, function(error, response, body) {
-				if(error) {
-					console.log("Api call failed!")
-					io.sockets.emit('new message',{msg : data, nick : socket.nickname, sentiment : ""});
-				}
-					else {
-				senti = JSON.parse(body)
-				sentiParsed = String(senti['result'])
-				console.log(sentiParsed);
-				io.sockets.emit('new message',{msg : data, nick : socket.nickname, sentiment : sentiParsed});
-				}
+				request(url, function(error, response, body) {
+					if(error) {
+						console.log("Api call failed!")
+						io.sockets.emit('new message',{msg : data, nick : socket.nickname, sentiment : ""});
+					}
+						else {
+					senti = JSON.parse(body)
+					sentiParsed = String(senti['result'])
+					console.log(sentiParsed);
+					io.sockets.emit('new message',{msg : data, nick : socket.nickname, sentiment : sentiParsed});
+					}
+				});
 			});
+			
 		}
 
 	});
