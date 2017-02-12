0b24ea2dd65039c44b8d793c5608793f6cb7ae5b
noobg1 <jeevu.art@gmail.com>
2017-02-12 16:12:50 +0530

    private messaging done

--- app.js
+++ app.js
@@ -13,48 +13,86 @@ app.get('/',function(req,res){
 	res.sendfile(__dirname + '/index.html');
 });
 
+// function makeRequest(data, socket.nickname) {
+// 	if( /^[a-zA-Z( )]+$/.test( data )) data = data;
+// 		else if ((data) == "") data = 'empty message';
+// 		else data = 'empty message';
+// 		var url = "http://noobg1.pythonanywhere.com/q/"+data, senti, sentiParsed
+
+// 		request(url, function(error, response, body) {
+// 			if(error) {
+// 				console.log("Api call failed!")
+// 				io.sockets.emit('new message',{msg : data, nick : socket.nickname, sentiment : ""});
+// 			}
+// 				else {
+// 			 senti = JSON.parse(body)
+// 			 sentiParsed = String(senti['result'])
+// 			console.log(sentiParsed);
+// 		  io.sockets.emit('new message',{msg : data, nick : socket.nickname, sentiment : sentiParsed});
+// 		}
+// 		});
+// }
+
 io.sockets.on('connection',function(socket){
-	socket.on('new user',function(data,callback){
+	socket.on('new user',function(data, callback){
 		if(data in users){
 			callback(false);
 		}else{
 			callback(true);
 			socket.nickname = data;
-			nicknames.push(socket.nickname);
+			users[socket.nickname] = socket;
 			updateNicknames();
 		}
 	});
 
-	socket.on('send message',function(data){
-		if( /^[a-zA-Z( )]+$/.test( data )) data = data;
-		else if ((data) == "") data = 'empty message';
-		else data = 'empty message';
-		var url = "http://noobg1.pythonanywhere.com/q/"+data, senti, sentiParsed
-
-		request(url, function(error, response, body) {
-			if(error) {
-				console.log("Api call failed!")
-				io.sockets.emit('new message',{msg : data, nick : socket.nickname, sentiment : ""});
-			}
+	socket.on('send message',function(data, callback){
+		data = data.trim();
+		if(data.substr(0,3) === '/w '){
+			data = data.substr(3);
+			let ind = data.indexOf(" ");
+			if(ind != -1){
+				let name = data.substring(0, ind);
+				data = data.substring(ind + 1);
+				if(name in users){
+					users[name].emit('whisper',{msg : data, nick : socket.nickname} );
+					//users[name].emit('whisper',{msg : data, nick : name } );
+					console.log("whisper")
+				}
 				else {
-			 senti = JSON.parse(body)
-			 sentiParsed = String(senti['result'])
-			console.log(sentiParsed);
-		  io.sockets.emit('new message',{msg : data, nick : socket.nickname, sentiment : sentiParsed});
+					callback('Error! Enter valid User!');
+				}
+			}else {
+				callback('Please enter a message!');
+			}
+		}else {
+			if( /^[a-zA-Z( )]+$/.test( data )) data = data;
+			else if ((data) == "") data = 'empty message';
+			else data = 'empty message';
+			var url = "http://noobg1.pythonanywhere.com/q/"+data, senti, sentiParsed
+
+			request(url, function(error, response, body) {
+				if(error) {
+					console.log("Api call failed!")
+					io.sockets.emit('new message',{msg : data, nick : socket.nickname, sentiment : ""});
+				}
+					else {
+				senti = JSON.parse(body)
+				sentiParsed = String(senti['result'])
+				console.log(sentiParsed);
+				io.sockets.emit('new message',{msg : data, nick : socket.nickname, sentiment : sentiParsed});
+				}
+			});
 		}
-		});
-		
-		
 
 	});
 
 	function updateNicknames(){
-		io.sockets.emit('username',nicknames);
+		io.sockets.emit('usernames',Object.keys(users));
 	}
 
 	socket.on('disconnect',function(data){
 		if(!socket.nickname) return;
-		nicknames.splice(nicknames.indexOf(socket.nickname),1);
+		delete users[socket.nickname];
 		updateNicknames();
 	})
 
